f8728f6cc46c3a128862a6e3ca41435e71415049
lynch.thom <lynch.thom@flux.cs.uwlax.edu>
2014-12-04 15:35:41 -0600

    Working multi-select - pre button configuration. Incorrect url for flux

--- server/reporters/js-proto-v1/Test/js/main.js
+++ server/reporters/js-proto-v1/Test/js/main.js
@@ -5,11 +5,20 @@
 
 $(document).ready(function() {
 
+    var hideColList = [];
+    var showColList = [];
+    var colList =  [];
+    var url = "ajax/data/pretty.json";
+    //            url: "http://flux.cs.uwlax.edu/~jjhursey/mtt-tmp/pretty.json",
+
+
+    /*
+        Table Configuration
+     */
     var table = $('#example').DataTable({
         "dom": 'C<"clear">Rlfrtip',
         "ajax": {
-//            url: "ajax/data/pretty.json",
-            url: "http://flux.cs.uwlax.edu/~jjhursey/mtt-tmp/pretty.json",
+            url: url,
             dataSrc: "values"
         },
         "columnDefs": [ {
@@ -22,6 +31,7 @@ $(document).ready(function() {
             restore: "Restore",
             showAll: "Show All",
             showNone: "Show None",
+            "buttonText": "Change columns",
 
             groups: [
                 {
@@ -52,34 +62,6 @@ $(document).ready(function() {
                     return title;
                 }
                 //TODO: Make label dynamic
-
-//                switch( index ){
-//                    case 0:
-//                    case 1:
-//                    case 2:
-//                    case 3:
-//                    case 4:
-//                    case 5:
-//                        return title;
-//                        break;
-//                    case 6:
-//                    case 7:
-//                        return "MPI Install: " + title;
-//                        break;
-//                    case 8:
-//                    case 9:
-//                        return "Test Build: " + title;
-//                        break;
-//                    case 10:
-//                    case 11:
-//                    case 12:
-//                    case 13:
-//                        return "Test Run: " + title;
-//                        break;
-//                    default:
-//                        return title;
-//                        break;
-//                }
             },
 
             stateChange: function (iColumn, bVisibile){
@@ -103,6 +85,143 @@ $(document).ready(function() {
     });
 
 
+    //TODO: dynamic table headers
+
+    function setHeaders( columnName ){
+        //var input = "<th rowspan=\"2\">" + columnName + "</th>";
+        //$( "#example" ).append( input );
+    }
+
+
+
+    /*
+        MultiSelect Initialization
+     */
+
+    $.ajax({
+        dataType: "json",
+        url: url
+    })
+        .done( function( data ){
+              for( var i = 0; i < data.fields.length; i++ ){
+                  setHeaders( data.fields[i] );
+                  colList.push( data.fields[i] );
+                  showColList.push( data.fields[i] );
+              }
+
+            fillColList();
+            buildSelect();
+        });
+
+
+    function fillColList(){
+
+        for( var i = 0; i < colList.length; i++  ) {
+            var name = colList[i];
+            var input = "<option value\"" + name + "\">" + name + "</option>";
+            $('#my-select').append( input );
+        }
+
+        console.log( table.data()[0][0]  );
+    }
+
+
+
+    /*
+        MultiSelect Configuration
+     */
+
+    function buildNewArray ( values, arr ){
+        var tmpArray = arr;
+        arr = [];
+
+        tmpArray.forEach( function(entry) {
+            if( values.toString() !== entry.toString() ){
+                arr.push( entry );
+            }
+        });
+
+        return arr;
+
+    }
+
+  function  buildSelect(){
+      $('#my-select').multiSelect({
+          selectableHeader: "<div class='custom-header'> Show Columns </div>",
+          selectionHeader: "<div class='custom-header'> Hide Columns </div>",
+
+          afterSelect: function( values ){
+              //Recreate show array
+              console.log("Show:");
+              showColList = buildNewArray( values, showColList );
+
+              showColList.forEach( function(entry) {
+                  console.log(entry);
+              });
+
+              //Push to hidden array
+              console.log("Hide:");
+              hideColList.push( values.toString() );
+
+              hideColList.forEach( function(entry) {
+                  console.log(entry);
+              });
+          },
+
+          afterDeselect: function( values ){
+              //Push to show array
+              console.log("Now Show:");
+              showColList.push( values.toString() );
+
+              showColList.forEach( function(entry) {
+                  console.log(entry);
+              });
+
+              //Recreate hidden array
+              console.log("Now Hide:");
+              hideColList = buildNewArray( values, hideColList );
+
+              hideColList.forEach( function(entry) {
+                  console.log(entry);
+              });
+          }
+
+      });
+  }
+
+    //TODO: convert strings to indexes
+    function getIndexes(){
+        //for( var i = 0; i < colList.length){
+        //
+        //}
+    }
+
+    //TODO: Aggregate data
+    //TODO: implement button to toggle visibility
+    function toggleCols(){
+
+        //for( var i = 0; i < showColList() ){
+        //
+        //}
+
+            //for all columns in show
+                //find column - make visible true
+            //for all columns in hide
+                //find column - make visible false
+
+        for( var i = 0; i < table.columns; i++ ){
+
+        }
+
+
+    }
+
+
+
+
+
+
+
     $('#example tbody').on( 'click', 'tr', function () {
         if ( $(this).hasClass('selected') ) {
             $(this).removeClass('selected');
@@ -113,13 +232,23 @@ $(document).ready(function() {
         }
     } );
 
-    $('#remove').click( function () {
-        table.row('.selected').remove().draw( false );
+    $( 'button[ name = "Change columns" ]' ).on( 'click', function(){
+        alert( "Hey! I was hit!!!" );
     } );
 
-//    $( "#remove" ).click( function() {
-//       alert("I do a thing!");
-//    });
+    $('#show-hide').click( function () {
+        //hideColList.forEach( function(entry){
+        //   console.log( entry );
+        //});
+        fillColList();
+    } );
+
+
+
+
+    //dropdown checkbox select
+    //populate with current columns
+    //
 
 
 
